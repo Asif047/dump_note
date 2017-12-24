96d7e4cb8bd4637f51c04d6f3a2af73142afa67e
Asif047 <asifulhaque047@gmail.com>
2017-12-24 19:25:15 +0600

    local branch 2 updated

---
+++ NoteMe/app/src/main/java/com/rokomari_poc/noteme/LogIn/LogInActivity.java
@@ -0,0 +1,15 @@
+package com.rokomari_poc.noteme.LogIn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rokomari_poc.noteme.R;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
