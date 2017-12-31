223417437ae33f5c2e4958f3969f82614a60d1fd
Asif047 <asifulhaque047@gmail.com>
2017-12-31 19:15:39 +0600

    local branch 9 updated

--- NoteMe/app/src/main/java/com/rokomari_poc/noteme/AllNotes/AllNotesActivity.java
+++ NoteMe/app/src/main/java/com/rokomari_poc/noteme/AllNotes/AllNotesActivity.java
@@ -1,11 +1,17 @@
 package com.rokomari_poc.noteme.AllNotes;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
+import com.ontbee.legacyforks.cn.pedant.SweetAlert.SweetAlertDialog;
+import com.rokomari_poc.noteme.AlertBox.ShowAlert;
 import com.rokomari_poc.noteme.R;
+import com.rokomari_poc.noteme.Utils.NetworkUtils.MyNetworkCheck;
 
 import java.util.List;
 
@@ -20,10 +26,30 @@ public class AllNotesActivity extends AppCompatActivity {
     private List<ModelNotes> modelNotes;
     private ApiInterfaceNotes apiInterfaceNotes;
 
+    private Toolbar toolbar;
+
+    static SweetAlertDialog pDialog;
+    private ShowAlert showAlert;
+    private MyNetworkCheck myNetworkCheck = new MyNetworkCheck();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_notes);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Setting toolbar as the ActionBar with setSupportActionBar() call
+        setSupportActionBar(toolbar);
+
+
+        pDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
+        showAlert=new ShowAlert(this);
+
+
+        pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+        pDialog.setTitleText("Loading");
+        pDialog.setCancelable(false);
+        pDialog.show();
 
         recyclerView=(RecyclerView)findViewById(R.id.recyclerview_all_notes);
         layoutManager=new LinearLayoutManager(this);
@@ -33,18 +59,30 @@ public class AllNotesActivity extends AppCompatActivity {
         apiInterfaceNotes=ApiClientNotes.getApiClient().create(ApiInterfaceNotes.class);
         Call<List<ModelNotes>> call=apiInterfaceNotes.getNotes();
 
-        call.enqueue(new Callback<List<ModelNotes>>() {
-            @Override
-            public void onResponse(Call<List<ModelNotes>> call, Response<List<ModelNotes>> response) {
-                modelNotes=response.body();
-                recyclerAdapterNotes=new RecyclerAdapterNotes(modelNotes);
-                recyclerView.setAdapter(recyclerAdapterNotes);
-            }
+        if (!myNetworkCheck.isConnected(AllNotesActivity.this))
+            showAlert.showWarningNetAllNotesActivity();
+        else
+        {
+            call.enqueue(new Callback<List<ModelNotes>>() {
+                @Override
+                public void onResponse(Call<List<ModelNotes>> call, Response<List<ModelNotes>> response) {
+                    if (pDialog.isShowing())
+                        pDialog.dismiss();
 
-            @Override
-            public void onFailure(Call<List<ModelNotes>> call, Throwable t) {
+                    modelNotes=response.body();
+                    //Log.e("####RESPONSE:",modelNotes.get(0).getCategory());
+                    recyclerAdapterNotes=new RecyclerAdapterNotes(AllNotesActivity.this,modelNotes);
+                    recyclerView.setAdapter(recyclerAdapterNotes);
+                }
 
-            }
-        });
+                @Override
+                public void onFailure(Call<List<ModelNotes>> call, Throwable t) {
+
+                    Log.e("####RESPONSE:","Failure");
+                }
+            });
+        }
+
+
     }
 }
---
+++ NoteMe/app/src/main/java/com/rokomari_poc/noteme/Register/RegisterMyPostRequest.java
@@ -0,0 +1,104 @@
+package com.rokomari_poc.noteme.Register;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.rokomari_poc.noteme.LogIn.LogInActivity;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class RegisterMyPostRequest {
+
+    private String responsePost = "";
+    private RegisterPostResponseApi registerPostResponseApi;
+    private String response_status;
+    private Context context;
+
+    //private PostActivity postActivity=new PostActivity();
+
+    public RegisterMyPostRequest(Context context) {
+        this.context = context;
+    }
+
+
+    public void postData(String fname, String lname, String mobile, String email, String password) {
+
+
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("first_name", "" + fname);
+        params.put("last_name", "" + lname);
+        params.put("email", "" + email);
+        params.put("mobile", "" + mobile);
+        params.put("password", "" + password);
+
+        JSONObject parameter = new JSONObject(params);
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(JSON, parameter.toString());
+        Request request = new Request.Builder()
+                .url("http://192.168.11.205:5001/register")
+                .post(body)
+                .header("token","68e109f0f40ca72a15e05cc22786f8e6")
+                .addHeader("content-type", "application/json; charset=utf-8")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                //Log.e("response", call.request().body().toString());
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                if (RegisterActivity.pDialog.isShowing())
+                    RegisterActivity.pDialog.dismiss();
+
+                responsePost = response.body().string();
+                Log.e("response", responsePost);
+
+                Gson gson = new Gson();
+                Type type = new TypeToken<RegisterPostResponseApi>() {
+                }.getType();
+
+
+                registerPostResponseApi = gson.fromJson(responsePost, type);
+                response_status = String.format("" + registerPostResponseApi.getStatus());
+
+
+                Log.d("RESPONSE POST: ", response_status);
+
+                if(response_status.equals("registered"))
+                {
+                    Intent intent = new Intent(context, LogInActivity.class);
+
+                    context.startActivity(intent);
+                }
+
+
+            }
+
+        });
+
+    }
+
+}
